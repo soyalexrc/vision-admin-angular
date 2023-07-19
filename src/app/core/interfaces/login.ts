f2eb0a1a49317b4097f89c8b2417d13ce711f407
alexc <alexcarvajal2404@gmail.com>
2023-07-19 12:20:40 -0500

    :tada: second commmit

---
+++ src/app/core/interfaces/login.ts
@@ -0,0 +1,32 @@
+export interface Login {
+  recordsets:   Array<Recordset[]>;
+  recordset:    Recordset[];
+  output:       Output;
+  rowsAffected: any[];
+}
+
+export interface Output {
+}
+
+export interface Recordset {
+  id:               number;
+  first_name:       string;
+  last_name:        string;
+  username:         string;
+  phone_number1:    string;
+  phone_number2:    string;
+  email:            string;
+  fiscal_address:   string;
+  birthday:         Date;
+  profession:       string;
+  city:             string;
+  state:            string;
+  user_type:        string;
+  social_facebook:  string;
+  social_twitter:   string;
+  social_instagram: string;
+  social_youtube:   string;
+  image:            null;
+  password:         string;
+  sequence:         null;
+}
