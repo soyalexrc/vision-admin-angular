c2a98a56765f90d6fd8e4269b7b5a61fdb55d748
alexrodriguez <alexcarvajal2404@gmail.com>
2024-01-03 16:39:11 -0500

    :zap: npm version updater prefixed,

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  apiUrl: 'http://148.135.39.118/:3000/api/v1'
+  apiUrl: 'http://148.135.39.118/:3000/api/v1',
+  appVersion: require('../../package.json').version,
 };
